ba2888a0f63b73479c9ccbc2499633d6ca61ae95
Boris DENOYELLE <bordenoy@e1r10p2.42.fr>
2021-02-01 17:12:39 +0100

    racine carre de 1 debug

--- philo_one/eat.c
+++ philo_one/eat.c
@@ -6,7 +6,7 @@
 /*   By: bordenoy <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/01 16:20:39 by bordenoy          #+#    #+#             */
-/*   Updated: 2021/02/01 16:40:00 by bordenoy         ###   ########.fr       */
+/*   Updated: 2021/02/01 17:03:11 by bordenoy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,4 +20,6 @@ void eat(t_philosophe *p)
 		take_lfork(p);
 	await(p->table->opt->tte);
 	unlock_fork(p);
+	p->eat++;
+	await(p->table->opt->tts);
 }
