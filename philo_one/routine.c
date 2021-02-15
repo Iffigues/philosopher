564af999d45e901c509aa432afbe2d050080a7a6
iiffigues <iffigues@vivaldi.net>
2021-02-15 18:23:04 +0100

    hacking of github repos

--- philo_one/routine.c
+++ philo_one/routine.c
@@ -3,14 +3,27 @@
 void routine_me(t_table *table)
 {
     int i;
+    int c;
 
     while (1)
     {
+        c = 1;
         i = 0;
         while (i < table->nb)
         {
+            pthread_mutex_lock(&table->philosofe[i].w);
+            if (table->philosofe[i].eat < table->me)
+                c = 0;
+            if (micros() > table->philosofe[i].await) {
+                    message(&table->philosofe[i]," died\n", micros() - table->start);
+                    return;
+            }
+            pthread_mutex_unlock(&table->dead);
+            pthread_mutex_unlock(&table->philosofe[i].w);
             i++;
         }
+        if (c)
+            exit(0);
     }
 }
 
