bf07ce8c7624288a2c0257651bf37e2794f5e7c9
Boris DENOYELLE <bordenoy@e1r13p2.42.fr>
2021-03-02 15:35:48 +0100

    hello

---
+++ philo-four/opt.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   opt.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bordenoy <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/26 15:08:09 by bordenoy          #+#    #+#             */
+/*   Updated: 2021/02/26 15:09:04 by bordenoy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "./philosopher.h"
+
+static int	opt_verif(t_table *l, int argc)
+{
+	if (l->nb <= 0 || l->nb > 200 || l->ttd <= 60)
+		return (0);
+	if (l->tts <= 60 || l->tte <= 60)
+		return (0);
+	if (argc == 6 && l->me <= 0)
+		return (0);
+	return (1);
+}
+
+t_table		*make_opt(int argc, char **argv)
+{
+	t_table *l;
+
+	if (argc == 1 || argc > 6 || argc < 5)
+		return (NULL);
+	if (!(l = (t_table*)malloc(sizeof(t_table))))
+		return (NULL);
+	l->nb = ft_atoi(argv[1]);
+	l->ttd = ft_atoi(argv[2]);
+	l->tte = ft_atoi(argv[3]) * 1000;
+	l->pair_wait = ft_atoi(argv[3]) / 2;
+	l->last_imp_wait = (1.5) * ft_atoi(argv[3]);
+	l->died = 1;
+	l->tts = ft_atoi(argv[4]) * 1000;
+	l->me = 0;
+	if (argc == 6)
+	{
+		l->me = ft_atoi(argv[5]);
+		sem_unlink("eat");
+		l->eat = sem_open("eat", O_CREAT | O_EXCL, 0644, l->nb);
+	}
+	if (opt_verif(l, argc) == 0)
+		return (NULL);
+	return (l);
+}
---
+++ philo-four/routine.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   routine.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bordenoy <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/26 12:41:22 by bordenoy          #+#    #+#             */
+/*   Updated: 2021/03/01 16:39:03 by bordenoy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "./philosopher.h"
+
+static void	*dier(t_philosophe *table)
+{
+	int i;
+
+	i = 0;
+	message(table, " died\n", micros() - table->table->start);
+	sem_post(table->w);
+	sem_post(table->table->dead);
+	table->table->died = 0;
+	return (NULL);
+}
+
+void		*routines(void *pp)
+{
+	t_philosophe *p;
+
+	p = (t_philosophe*)pp;
+	p->await = micros() + p->table->ttd;
+	while (p->table->died)
+	{
+		sem_wait(p->w);
+		if (micros() > p->await)
+			return (dier(p));
+		if (p->table->me > 0)
+			if (p->eat >= p->table->me)
+			{
+				sem_post(p->table->eat);
+				p->table->died = 0;
+				return (NULL);
+			}
+		sem_post(p->w);
+		usleep(1000);
+	}
+	return (NULL);
+}
